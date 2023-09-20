695dd5d81c92f4277a12cc79473f0845c2072920
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-09-20 16:19:45 +0300

    Methods are created for Person class

--- src/com/workintech/d3oop/Person.java
+++ src/com/workintech/d3oop/Person.java
@@ -7,6 +7,18 @@ public class Person {
     double height;
     String hairColor;
     boolean hasGlasses;
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public int getAge() {
+        return age;
+    }
+    public boolean isTeen() {
+        return age <= 19 && age >= 13;
+    }
 
     public Person(String firstName, String lastName, int age) {
         this.firstName = firstName;
